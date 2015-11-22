2dbb3d58efec0a38965edafa0a5512807c032145
Frank Roth <frankred@web.de>
2015-11-22 16:44:00 +0100

    Initial import:

---
+++ medienwelt.mm
@@ -0,0 +1,341 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1448197418091" ID="ID_1040596314" MODIFIED="1448204399960" TEXT="Medienmanipulation">
+<edge STYLE="sharp_bezier"/>
+<node CREATED="1448197584699" ID="ID_796987162" MODIFIED="1448198351724" POSITION="right" TEXT="Zeitverlag">
+<edge STYLE="sharp_bezier"/>
+<node CREATED="1448197457717" ID="ID_771029270" MODIFIED="1448198351723">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/die-zeit.png" />
+  </body>
+</html></richcontent>
+<edge STYLE="sharp_bezier"/>
+<node CREATED="1448198027961" ID="ID_1401568932" MODIFIED="1448204917384" TEXT="Auflage: ~500.000">
+<edge STYLE="sharp_bezier"/>
+</node>
+<node CREATED="1448198606586" ID="ID_25846396" MODIFIED="1448198699195" TEXT="Herrausgeber">
+<node CREATED="1448197488602" ID="ID_291397959" MODIFIED="1448202008189">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <img src="images/people/josef-josse-small.png" />
+      
+    </p>
+    <p style="text-align: center">
+      Josef Joffe
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1448198941061" ID="ID_1985979604" MODIFIED="1448199857930" TEXT="Journalistische Interessenkonflikte Joffes durch seine unstreitigen Mitgliedschaften in atlantischen Lobby-Organisationen"/>
+<node CREATED="1448199816592" ID="ID_1197095067" MODIFIED="1448200091955">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mitglied:
+    </p>
+    <p>
+      <img src="images/organisations/the-aspen-institute.png" />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1448200216913" ID="ID_1097843316" MODIFIED="1448200251366">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mitglied:
+    </p>
+    <p>
+      <img src="images/organisations/global-jewish-advocacy.png" />
+      
+    </p>
+    <p>
+      (American Jewish Committee)
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1448202010227" ID="ID_1021010948" MODIFIED="1448202011926" TEXT="Das American Jewish Committee (deutsch: Amerikanisch-J&#xfc;disches Komitee) ist eine US-Organisation, die ihre Aufgabe darin sieht,  &#x201e;Besch&#xfc;tzer des Wohls und der Sicherheit der Juden in den USA, in Israel und der ganzen Welt zu sein; die Prinzipien des Pluralismus als beste Verteidigung gegen Anti-Semitismus und andere Engstirnigkeit weltweit zu st&#xe4;rken und die Qualit&#xe4;t j&#xfc;dischen Lebens in Amerika durch Sicherung des j&#xfc;dischen Fortbestehens und Vertiefung der Bindungen zwischen amerikanischen und israelischen Juden zu verbessern.&#x201c; "/>
+<node CREATED="1448200571475" ID="ID_536472874" MODIFIED="1448200633016">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Webseite: <a href="http://www.ajc.org/">http://www.ajc.org/</a>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1448200642214" ID="ID_804425120" MODIFIED="1448200674833">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Youtube: <a href="https://www.youtube.com/user/AJCGlobal">https://www.youtube.com/user/AJCGlobal</a>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1448200696809" ID="ID_279527198" MODIFIED="1448200735314">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Twitter: <a href="https://twitter.com/ajcglobal">https://twitter.com/ajcglobal</a>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1448200919101" ID="ID_443940238" MODIFIED="1448200944091">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Google+: <a href="https://plus.google.com/+AjcOrg/posts">https://plus.google.com/+AjcOrg/posts</a>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1448201029449" ID="ID_24376977" MODIFIED="1448201038662" TEXT="Facts">
+<node CREATED="1448201039714" ID="ID_1000444633" MODIFIED="1448201123834" TEXT="4. Mai 2006: Angela Merkel tritt als Redner zum hundertsten Jahrestag auf."/>
+<node CREATED="1448201142931" ID="ID_1947222435" MODIFIED="1448201274011" TEXT="4. Mai 2006: Zum Jubil&#xe4;um sammelte die Vereinigung in 5 Jahre 105 Millionen US-Dollar"/>
+<node CREATED="1448202117866" ID="ID_269252054" MODIFIED="1448203616690">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      AJC betreibt die Organisation <a href="https://de.wikipedia.org/wiki/UN_Watch">UN WATCH</a>.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1448202366381" ID="ID_1524703832" MODIFIED="1448202437735">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>F&#252;hrung</b>: Alfred H. Moses, ehm. US-Botschafter
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1448202296534" ID="ID_1562306534" MODIFIED="1448202420176">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Hauptaufgabe</b>: Kritische Untersuchung der UN Beschl&#252;sse
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1448202269014" ID="ID_1321078099" MODIFIED="1448202824525" TEXT="Will die UN reformieren"/>
+</node>
+<node CREATED="1448203231963" ID="ID_1994432287" MODIFIED="1448203302221">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Aktivit&#228;ten</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1448202791482" ID="ID_886743442" MODIFIED="1448203228452">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &quot;UN-Unterkommission f&#252;r F&#246;rderung und Schutz der Menschenrechte&quot; beschuldigt Israel die Menschenrechte im&#160;Libanonkrieg 2006 verletzt zu haben. Daraufhin bezichtigte UN WATCH die Unterkommission der Mandats&#252;berschreitung.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1448203239882" ID="ID_369783046" MODIFIED="1448203291097" TEXT="Anregung von E-Mail-Aktionen an UN-Vertreter bez&#xfc;glich Hisbollah, Holocaustleugnung im Iran und antisemitischer/antichristlicher Lehrb&#xfc;cher in &#xc4;gypten und Saudi-Arabien."/>
+</node>
+<node CREATED="1448204176741" ID="ID_1412127269" MODIFIED="1448204239441">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Mein Kommentar</b>:&#160;&#160;UN WATCH&#160;&#160;gibt zwar vor die UN hinsichtlich der Einhaltung der Menschenrechte zu &#252;berwachen vertritt jedoch h&#228;ufig nur eigene Interessen, n&#228;mlich die der AJC.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1448201371932" ID="ID_1798528793" MODIFIED="1448203612414">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><u>Mein Kommentar</u></b>:&#160;Schaut man auf die verschiedenen Webpr&#228;senzen und sozialen Netzwerke werden die Interessen dieser Organisation schnell ersichtlich.
+    </p>
+    <p>
+      Proamerikanische Medienhetze gegen die muslimische Welt und gegen diejenigen die &#246;ffentlich Kritik &#252;ber Israel aus&#252;ben...
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Aber macht euch dazu am Besten selbst ein Bild!
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1448197945559" ID="ID_39626035" MODIFIED="1448198424805" TEXT="Gesch&#xe4;ftsf&#xfc;hrer: Rainer Esser">
+<edge STYLE="sharp_bezier"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="male2"/>
+</node>
+</node>
+<node CREATED="1448204401858" ID="ID_1667091949" MODIFIED="1448204466865" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <img src="images/organisations/axel-springer-se.png" />
+      
+    </p>
+    <p style="text-align: center">
+      Axel Springer SE
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1448204613009" ID="ID_1336761611" MODIFIED="1448204820168">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Unternehmensgrunds&#228;tze
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1448204664263" ID="ID_527023920" MODIFIED="1448204670139" TEXT="Das unbedingte Eintreten f&#xfc;r den freiheitlichen Rechtsstaat Deutschland als Mitglied der westlichen Staatengemeinschaft und die F&#xf6;rderung der Einigungsbem&#xfc;hungen der V&#xf6;lker Europas."/>
+<node CREATED="1448204686980" ID="ID_1464129471" MODIFIED="1448204735041" TEXT="Die Unterst&#xfc;tzung des transatlantischen B&#xfc;ndnisses und die Solidarit&#xe4;t in der freiheitlichen Wertegemeinschaft mit den Vereinigten Staaten von Amerika.">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1448204673810" ID="ID_55125162" MODIFIED="1448204718641" TEXT="Das Herbeif&#xfc;hren einer Auss&#xf6;hnung zwischen Juden und Deutschen, hierzu geh&#xf6;rt auch die Unterst&#xfc;tzung der Lebensrechte des israelischen Volkes.">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1448204692693" ID="ID_261449657" MODIFIED="1448204693409" TEXT="Die Ablehnung jeglicher Art von politischem Totalitarismus."/>
+<node CREATED="1448204697752" ID="ID_1328268697" MODIFIED="1448204755354" TEXT="Die Verteidigung der freien sozialen Marktwirtschaft.">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1448204829034" ID="ID_424207050" MODIFIED="1448204838306" TEXT="Medien">
+<node CREATED="1448204469607" ID="ID_437041137" MODIFIED="1448204533506">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <img src="images/media/bild.png" />
+      
+    </p>
+    <p style="text-align: center">
+      Bild (Zeitung)
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1448204905872" ID="ID_1419768647" MODIFIED="1448204927216" TEXT="Auflage: ~2.000.000"/>
+</node>
+<node CREATED="1448204558299" ID="ID_1761753108" MODIFIED="1448204567693">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/media/die-welt.png" />
+  </body>
+</html></richcontent>
+<node CREATED="1448204939021" ID="ID_246619101" MODIFIED="1448204947639" TEXT="Auflage: ~190.000"/>
+</node>
+<node CREATED="1448204590036" ID="ID_1509142161" MODIFIED="1448204608262">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/media/fakt.png" />
+  </body>
+</html></richcontent>
+<node CREATED="1448205041687" ID="ID_640176861" MODIFIED="1448205056643" TEXT="Polnische Boulevardzeitung"/>
+<node CREATED="1448205017423" ID="ID_50194721" MODIFIED="1448205038321" TEXT="Auflage: ~390.000 "/>
+</node>
+</node>
+<node CREATED="1448205561513" ID="ID_35501353" MODIFIED="1448205563997" TEXT="Gr&#xfc;nder">
+<node CREATED="1448205286229" ID="ID_750437254" MODIFIED="1448205567479">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <img src="images/people/axel-springer-small.png" />
+      
+    </p>
+    <p style="text-align: center">
+      Axel Springer
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1448205294958" ID="ID_1697888077" MODIFIED="1448205297238" TEXT="Freimaurer">
+<node CREATED="1448205361456" ID="ID_1417121037" MODIFIED="1448205362730" TEXT=" &#xbb;Wir m&#xfc;ssen in den Aufbau der Bundesrepublik freimaurerisches Gedankengut einbringen. Am besten w&#xe4;re, wir w&#xfc;rden unsere Verantwortlichen in einer Loge zusammenschlie&#xdf;en.&#xab;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
